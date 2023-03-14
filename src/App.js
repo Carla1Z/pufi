c16d3950ac7a0aac6ca1596ee8fbe1ed3f8044ae
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-14 18:23:03 -0300

    acomodo archivos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./globalStyles.scss";
 import { LandingPage } from "./views/LandingPage";
 import { PufiRain } from "./views/PufiRain";
 import { PufiPuff } from "./views/PufiPuff";
 import { PufiCart } from "./views/PufiCart";
 import { PufiNap } from "./views/PufiNap";
+import "./styles/globalStyles.scss";
 
 function App() {
   return (
--- src/views/LandingPage/index.jsx
+++ src/views/LandingPage/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar } from "../../components/Navbar";
-import "./styles.scss";
+import "../../styles/landingpage.scss";
 
 export const LandingPage = () => {
   return (
