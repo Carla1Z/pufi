3d2841604a987c536ea6f7e3f918c8826840413a
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-15 11:52:21 -0300

    -navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 import logo from "../../assets/logo.png";
-import "./styles.scss";
+import cart from "../../assets/cart.png";
+import nap from "../../assets/nap.png";
+import puff from "../../assets/puff.png";
+import rain from "../../assets/rain.png";
+import { IoIosArrowDown } from "react-icons/io";
+import "../../styles/navbar.scss";
 
 export const Navbar = () => {
   const [width, setWidth] = useState(window.innerWidth);
@@ -15,24 +20,46 @@ export const Navbar = () => {
     <div className="navbar">
       <img src={logo} alt="logo" />
       <ul className="menu">
-        <Link to="puff">
-          <li>PUFI PUFF</li>
-        </Link>
+        <li>
+          <Link to="puff">
+            <span className="spannav">
+              <img src={puff} alt="pufi puff" />
+              PUFI PUFF
+            </span>
+          </Link>
+        </li>
 
-        <Link to="rain">
-          <li>PUFI RAIN</li>
-        </Link>
+        <li>
+          <Link to="rain">
+            <span className="spannav">
+              <img src={rain} alt="pufi puff" />
+              PUFI RAIN
+            </span>
+          </Link>
+        </li>
 
-        <Link to="cart">
-          <li>PUFI CART</li>
-        </Link>
+        <li>
+          <Link to="cart">
+            <span className="spannav">
+              <img src={cart} alt="pufi cart" />
+              PUFI CART
+            </span>
+          </Link>
+        </li>
 
-        <Link to="nap">
-          <li>PUFI NAP</li>
-        </Link>
+        <li>
+          <Link to="nap">
+            <span className="spannav">
+              <img src={nap} alt="pufi nap" />
+              PUFI NAP
+            </span>
+          </Link>
+        </li>
       </ul>
-      <span>
-        <p>Mi cuenta</p>
+      <span className="user">
+        <p>Mi cuenta
+          <IoIosArrowDown />
+        </p>
         <p>Mi compra</p>
       </span>
     </div>
